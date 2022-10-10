2293a5c3cb3c0a4dfb5aeb042426971c1548c0c0
tyler ambrose <blitzinc98@yahoo.com>
2022-10-10 14:55:14 -0400

    Finished both exercises

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -21,6 +21,40 @@
                 Console.WriteLine("Nevermind");
             }
 
+            
+            Console.WriteLine("What is your favorite subject");
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                Console.WriteLine("math is cool");
+                break;
+
+                case "English":
+                    Console.WriteLine("English kinda sucks");
+                    break;
+
+                case "Science":
+                    Console.WriteLine("Science is fun");
+                    break;
+
+                case "Music":
+                    Console.WriteLine("Music isnt a real subject");
+                    break;
+
+                case "Physical Education":
+                    Console.WriteLine("Clearly the GOAT");
+                    break;
+
+                    default:
+                    Console.WriteLine("Never heard of that");
+                    break;
+
+            }
+            
+
+
         }
     }
 }
