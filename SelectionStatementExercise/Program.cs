a62e23609b7c7eb821e552398a11bb0027b4bc4f
tyler ambrose <blitzinc98@yahoo.com>
2022-10-10 14:21:09 -0400

    If statment done

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Can you guess my favorite number");
+            var userinput = int.Parse(Console.ReadLine());
+            int favoriteNumber = 100;
+            if (userinput > favoriteNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            if (userinput < favoriteNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
         }
     }
 }
+
+    
